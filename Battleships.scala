4845e3ca490a08b481b90fa058db84afe91116e8
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-04-01 19:40:58 +0100

    Managed to get gui to display

--- Battleships.scala
+++ Battleships.scala
@@ -2,6 +2,6 @@ object BattleShips {
     val display = new Display();
     def main (args: Array[String]){
         println("Welcome to battle ships!")
-        // display.top()
+        display.open()
     }
 }

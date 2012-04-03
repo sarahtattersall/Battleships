6755733df80fbb841939b5f80c4874ce7778376c
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-04-03 20:34:59 +0100

    Made GridCell a case class too and set its default status to Empty

--- src/main/scala/Display.scala
+++ src/main/scala/Display.scala
@@ -4,12 +4,11 @@ import java.awt.Dimension
 import java.awt.Color
 import java.awt.event.MouseAdapter
 
-class GridCell (s: BoardStatus) extends Button {
+case class GridCell (var status: BoardStatus = Empty) extends Button {
   preferredSize = new Dimension (20, 20)
   background = Color.RED
   text = "X"
   opaque = true
-  def status = s
   listenTo (mouse.clicks)
   reactions += {
     case mouseClicked => {
@@ -23,7 +22,7 @@ class Display (size: Int) extends MainFrame {
   def gridSize = size
   title = "Battleships"
   val gridPanel = new GridPanel(size, size){
-    contents.insertAll (0, Buffer.tabulate(gridSize * gridSize)(x => new GridCell(Empty)))
+    contents.insertAll (0, Buffer.tabulate(gridSize * gridSize)(x => new GridCell()))
   }
   contents = gridPanel
 }

89922d158ba7452131b8d58816bede17ec55102c
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-04-22 14:43:06 +0100

    Modified players to do all the setting up of the board. Perhaps have builder pattern?

--- src/main/scala/Players.scala
+++ src/main/scala/Players.scala
@@ -1,14 +1,76 @@
-abstract class Player(private val boardSize: Int) {
+abstract class Player(private val board: Board) {
+  protected var ships = List[Ship]()
+  
   def guess(): Coord
-}
-case class HumanPlayer(boardSize: Int) extends Player(boardSize) {
-  private val io = new IO()
-  def guess (): Coord = {
-    io.getCoord(0, boardSize)
+    
+  def getBoard (): Board = {
+    board
   }
+  
+  def alive (): Boolean = {
+    // TODO: WHy does _.sank() not work here?
+    ships.exists((x) => !x.sank())
+  }
+  
+  // Creates a ship of size size begining at start and going in direction
+  // direction.
+  // Returns the new list ship :: ships
+  protected def placeShip (size: Int, s: Coord, direction: Vec, coordSpace: CoordinateSpace): List[Ship] = {
+    var start = s
+    val ship = new Ship(size)
+    for (i <- 0 to size - 1){
+      board.updateShip(start, ship.getPiece(i))
+      start = coordSpace.move(start, direction)
+    }
+    (ship :: ships)
+  }
+  
 }
-case class AIPlayer(boardSize: Int) extends Player(boardSize) {
+case class HumanPlayer(board: Board) extends Player(board) {
+  private val io = new IO()
+  
+  def guess (): Coord = {
+    io.getCoord(0, board.size)
+  }
+  
+  // TODO: Ensure can't place ontop of another ship, for now assume the user
+  // doesn't
+  // Creates a new ship and places it on the playerBoard
+  def setupShip (size: Int, coordSpace: CoordinateSpace) {
+    val sizeIndex = size - 1
+    println ("Placing your ship of size " + size)
+    val start = io.getCoord(0, board.size)
+    val direction = io.getVector()
+    if (!inBoundary(board, start.x, direction.i*sizeIndex) 
+        || !inBoundary(board, start.y, direction.j*sizeIndex)){
+      println ("Placing of ship here goes out of bounds, try again")
+      setupShip (size, coordSpace)
+    } else {
+      ships = placeShip (size, start, direction, coordSpace)
+    }
+  }
+
+  // Checks if x is within the board boundary
+  private def inBoundary(board: Board, x: Int, offset: Int): Boolean = {
+    val y = x + offset
+    (y >= 0 && y < board.size)
+  }  
+}
+case class AIPlayer(board: Board) extends Player(board) {
+
   def guess (): Coord = {
     new Coord (0,0)
+  }
+  
+    
+  // TODO: Come up with an algorithm to place ships
+  // Creates a basic AIBoard with ships placed.
+  def setupBoard (sizes: List[Int], coordSpace: CoordinateSpace) {
+    val dir = new Vec(0,1)
+    sizes.foldLeft(0)((acc, size) => 
+      {
+        ships = placeShip (size, new Coord(acc, 0), dir, coordSpace)
+        acc + 1
+       })
   }
 }
--- src/main/scala/Rules.scala
+++ src/main/scala/Rules.scala
@@ -4,26 +4,26 @@ case object Miss extends Result
 
 class Rules (size: Int) {
   private val io = new IO()
-  private val playerBoard = new Board(size)
-  private val AIBoard = new Board(size)
   private val coordSpace = new CoordinateSpace(size, size)
+  private val AIPlayer = new AIPlayer(new Board(size))
+  private val humanPlayer = new HumanPlayer(new Board(size))
   private val sizes = List(5, 4, 3, 3, 2)
   private val fleet = Array.tabulate(sizes.size)(x => new Ship(sizes(x)))
-  private var playerShips = List[Ship]()
-  private var AIShips = List[Ship]()
-  createAIBoard()
   
   // Displays the players board and then calls setupShip for each
   // of the available ships with sizes dictated in sizes
   def setup () {
-    println (playerBoard.toString())
-    sizes.foreach(setupShip(_))
+    AIPlayer.setupBoard(sizes, coordSpace)
+    println (humanPlayer.getBoard().toString())
+    sizes.foreach((x) => {
+                    humanPlayer.setupShip(x, coordSpace)
+                    println(humanPlayer.getBoard().toString())
+                    })
   }
   
   
   def play(){
-    // TODO: WHy does _.sank() not work here?
-    while (playerShips.exists((x) => !x.sank()) && AIShips.exists((x) => !x.sank())){
+    while (humanPlayer.alive() && AIPlayer.alive()){
       
     }
   }
@@ -39,55 +39,5 @@ class Rules (size: Int) {
       }
       case _ => Miss
     }
-  }
-  
-  // Checks if x is within the board boundary
-  private def inBoundary(board: Board, x: Int, offset: Int): Boolean = {
-    val y = x + offset
-    (y >= 0 && y < board.size)
-  }
-  
-  // TODO: Ensure can't place ontop of another ship, for now assume the user
-  // doesn't
-  // Creates a new ship and places it on the playerBoard
-  private def setupShip (size: Int) {
-    val sizeIndex = size - 1
-    println ("Placing your ship of size " + size)
-    val start = io.getCoord(0, playerBoard.size)
-    val direction = io.getVector()
-    if (!inBoundary(playerBoard, start.x, direction.i*sizeIndex) 
-        || !inBoundary(playerBoard, start.y, direction.j*sizeIndex)){
-      println ("Placing of ship here goes out of bounds, try again")
-      setupShip (size)
-    } else {
-      playerShips = placeShip(playerBoard, playerShips, size, start, direction)
-      println(playerBoard.toString())
-    }
-  }
-  
-  // Creates a ship of size size begining at start and going in direction
-  // direction.
-  // Returns the new list ship :: ships
-  private def placeShip (board: Board, ships: List[Ship], size: Int, s: Coord, direction: Vec): List[Ship] = {
-    var start = s
-    val ship = new Ship(size)
-    for (i <- 0 to size - 1){
-      board.updateShip(start, ship.getPiece(i))
-      start = coordSpace.move(start, direction)
-    }
-    (ship :: ships)
-  }
-  
-  // TODO: Come up with an algorithm to place ships
-  // Creates a basic AIBoard with ships placed.
-  private def createAIBoard () {
-    val dir = new Vec(0,1)
-    sizes.foldLeft(0)((acc, size) => 
-      {
-        AIShips = placeShip(AIBoard, AIShips, size, new Coord(acc, 0), dir)
-        acc + 1
-       })
-    println("AI BOARD")
-    println(AIBoard.toString())
   }
 }
